94cdbd8ad61cc0058de6b2512519acd0eb8a599f
Nishant Maurya <nishantmaurya@Nishants-MacBook-Air.local>
2021-12-22 01:59:32 +0530

    Handle Errors outside express : 'unhandledRejection and uncaughtException in server.js'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// Local modules
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/AppError');
@@ -8,19 +9,19 @@ const globalErrorHandler = require('./utils/globalErrorHandler');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
+// Middlewares
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+// Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) =>
   next(new AppError(`Route ${req.originalUrl} not defined on server`, 404))
 );
 
+// Global error handler middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// Must be at the top : Event listener should be defined before event i.e error in synchronous code in 'uncaughtException'
+// Example - console.log(x) , x is not defined in our synchronous code, will be handled by this
+// Must exit the process when caught uncaughtException
+process.on('uncaughtException', (err) => {
+  console.log(`UNCAUGHT EXCEPTION! ${err.name} : ${err.message}`);
+  console.log(`Shutting down...`);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -9,6 +18,18 @@ const DBString = process.env.DB.replace('<password>', process.env.DB_PASS);
 mongoose.connect(DBString).then(() => console.log('Database connected'));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App started on port : ${PORT}`);
+});
+
+// Handle "unhandled rejection in async code"
+process.on('unhandledRejection', (err) => {
+  console.log(`UNHANDLED REJECTION! ${err.name} : ${err.message}`);
+
+  // Handle all running http requests and close the server, then exit the process
+  server.close(() => {
+    console.log(`Server closed. Shutting down...`);
+    // Exit current process, so that third party service can restart our node app
+    process.exit(1);
+  });
 });
